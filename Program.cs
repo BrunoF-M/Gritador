95abc8facefa5bf9e52a3a7da8be51f637d6d351
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-11 21:15:56 -0300

    resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textodigitado;
+
+            Console.WriteLine("digite algo... ");
+            textodigitado = Console.ReadLine();
+            Console.WriteLine($"{textodigitado}".ToUpper());
+        }
+    }
+}
